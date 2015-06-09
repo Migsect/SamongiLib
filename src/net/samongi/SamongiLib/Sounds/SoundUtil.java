ef871b34b33c18d55a2334312fdb28e6c61d35e0
Migsect <alsamon@yahoo.com>
2015-06-09 09:20:46 -0400

    Update 0.12
    + Started soundutil class, nothing it it yet.

---
+++ src/net/samongi/SamongiLib/Sounds/SoundUtil.java
@@ -0,0 +1,12 @@
+package net.samongi.SamongiLib.Sounds;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class SoundUtil
+{
+  public static void playSound(Location location, Player player, String sound, float volume, float pitch)
+  {
+    
+  }
+}
