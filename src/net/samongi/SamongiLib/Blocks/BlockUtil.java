8ac3427f3523a010bfc591101df3a3de9b751e4f
Migsect <alsamon@yahoo.com>
2016-11-08 23:49:43 -0500

    Added the base for invalid configuration exceptions.

--- src/net/samongi/SamongiLib/Blocks/BlockUtil.java
+++ src/net/samongi/SamongiLib/Blocks/BlockUtil.java
@@ -10,64 +10,71 @@ import org.bukkit.util.Vector;
 
 public class BlockUtil
 {
-	/**Finds a chain of blocks that match together
-	 * 
-	 * @param block The block to find all connecting blocks
-	 * @return A list of all blocks found.
-	 */
-	public static List<Block> getBlockChain(Block block, int max_distance)
-	{
-		List<Block> found_blocks = new ArrayList<>();
-		checkNeighbors(block, found_blocks, max_distance);
-		return found_blocks;
-	}
-	
-	/**Checks neighbors for similar blocks
-	 * 
-	 * @param block The block that is being checked around.
-	 * @param found_blocks Blocks that should not be included in the checks.
-	 */
-	private static void checkNeighbors(Block block, List<Block> found_blocks, int max_distance)
-	{
-		if(max_distance < 0) return;
-		List<Block> to_check = new ArrayList<>();
-		for(int x = -1; x <= 1 ; x++)
-		{
-			for(int y = -1; y <= 1 ; y++)
-			{
-				for(int z = -1; z <= 1 ; z++)
-				{
-					if(x == 0 && y == 0 && z == 0) continue;
-					Block relative_block = block.getRelative(x, y, z);
-					if(relative_block.getType().equals(block.getType())) to_check.add(block);
-				}
-			}
-		}
-		for(Block b : to_check)
-		{
-			found_blocks.add(b);
-			checkNeighbors(b, found_blocks, max_distance - 1);
-		}
-		if(to_check.size() == 0) return;
-	}
-  
-	public static boolean hasBlocksBetween(Location loc1, Location loc2)
-	{
-	  Vector starting_vector = loc1.toVector();
-	  Vector direction_vector = loc1.toVector().subtract(loc2.toVector());
-	  BlockIterator iterator = new BlockIterator(loc1.getWorld(), starting_vector, direction_vector, 0, (int) Math.ceil(loc1.toVector().distance(loc2.toVector())));
-	  while(iterator.hasNext())
-	  {
-	    if(iterator.next().getType().isSolid()) return true;
-	  }
-	  return false;
-	}
-	
-	public static Location center(Location block_location)
-	{
-	  double x = block_location.getBlockX() + 0.5;
-	  double y = block_location.getBlockY() + 0.5;
-	  double z = block_location.getBlockZ() + 0.5;
-	  return new Location(block_location.getWorld(), x, y, z);
-	}
+
+  /**
+   * Finds a chain of blocks that match together
+   * 
+   * @param block
+   *          The block to find all connecting blocks
+   * @return A list of all blocks found.
+   */
+  public static List<Block> getBlockChain(Block block, int max_distance)
+  {
+    List<Block> found_blocks = new ArrayList<>();
+    checkNeighbors(block, found_blocks, max_distance);
+    return found_blocks;
+  }
+
+  /**
+   * Checks neighbors for similar blocks
+   * 
+   * @param block
+   *          The block that is being checked around.
+   * @param found_blocks
+   *          Blocks that should not be included in the checks.
+   */
+  private static void checkNeighbors(Block block, List<Block> found_blocks, int max_distance)
+  {
+    if (max_distance < 0) return;
+    List<Block> to_check = new ArrayList<>();
+    for (int x = -1; x <= 1; x++)
+    {
+      for (int y = -1; y <= 1; y++)
+      {
+        for (int z = -1; z <= 1; z++)
+        {
+          if (x == 0 && y == 0 && z == 0) continue;
+          Block relative_block = block.getRelative(x, y, z);
+          if (relative_block.getType().equals(block.getType())) to_check.add(block);
+        }
+      }
+    }
+    for (Block b : to_check)
+    {
+      found_blocks.add(b);
+      checkNeighbors(b, found_blocks, max_distance - 1);
+    }
+    if (to_check.size() == 0) return;
+  }
+
+  public static boolean hasBlocksBetween(Location loc1, Location loc2)
+  {
+    Vector starting_vector = loc1.toVector();
+    Vector direction_vector = loc1.toVector().subtract(loc2.toVector());
+    BlockIterator iterator = new BlockIterator(loc1.getWorld(), starting_vector, direction_vector, 0,
+        (int) Math.ceil(loc1.toVector().distance(loc2.toVector())));
+    while (iterator.hasNext())
+    {
+      if (iterator.next().getType().isSolid()) return true;
+    }
+    return false;
+  }
+
+  public static Location center(Location block_location)
+  {
+    double x = block_location.getBlockX() + 0.5;
+    double y = block_location.getBlockY() + 0.5;
+    double z = block_location.getBlockZ() + 0.5;
+    return new Location(block_location.getWorld(), x, y, z);
+  }
 }
---
+++ src/net/samongi/SamongiLib/Exceptions/InvalidConfigurationException.java
@@ -0,0 +1,12 @@
+package net.samongi.SamongiLib.Exceptions;
+
+public class InvalidConfigurationException extends Exception
+{
+
+  private static final long serialVersionUID = -4042722823300063700L;
+
+  public InvalidConfigurationException(String message)
+  {
+    super(message);
+  }
+}
