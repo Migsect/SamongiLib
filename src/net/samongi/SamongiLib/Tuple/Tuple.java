b9560c65fa1e36e6af161231df67afedd0f3207d
Alex Sammons <alsamong@gmail.com>
2020-03-07 00:12:53 -0500

    Fixed the tuples a little

--- src/net/samongi/SamongiLib/Tuple/Tuple.java
+++ src/net/samongi/SamongiLib/Tuple/Tuple.java
@@ -1,69 +1,81 @@
 package net.samongi.SamongiLib.Tuple;
 
 public class Tuple {
-    private final Class<?>[]m_classes;
+
     private final Object[] m_values;
 
     public Tuple(Object... values) {
         m_values = values;
-        m_classes = new Class<?>[m_values.length];
-        for(int index = 0; index < m_values.length; index++)
-        {
-            m_classes[index] = m_values[index].getClass();
-        }
     }
 
-    public <T> T get(int index)
-    {
+    public <T> T get(int index) {
         return (T) m_values[index];
     }
 
-    public int size()
-    {
+    public int size() {
         return m_values.length;
     }
 
-    public Tuple range(int start)
-    {
+    public Tuple range(int start) {
         return range(start, m_values.length);
     }
 
-    public Tuple range(int start, int end)
-    {
-        if(end > m_values.length || start < 0)
-        {
+    public Tuple range(int start, int end) {
+        if (end > m_values.length || start < 0) {
             throw new IndexOutOfBoundsException();
         }
 
         int length = end - start;
         Object[] range = new Object[length];
-        for(int index = 0; index < length; index++)
-        {
+        for (int index = 0; index < length; index++) {
             range[index] = m_values[index + start];
         }
         return new Tuple(range);
     }
 
     public boolean equals(Tuple other) {
-        if(other.size() != size()) {
+        if (other.size() != size()) {
             return false;
         }
-        for(int index = 0; index < m_values.length; index++){
-            if(!m_values[index].equals(other.m_values[index]))
-            {
+        for (int index = 0; index < m_values.length; index++) {
+            if (m_values[index] == null && other.m_values[index] == null) {
+                continue;
+            }
+            if (m_values[index] == null || other.m_values[index] == null) {
+                return false;
+            }
+            if (!m_values[index].equals(other.m_values[index])) {
                 return false;
             }
         }
         return true;
     }
+
+    @Override public String toString() {
+        String outputString = "[";
+        int index = 0;
+        for (Object object : m_values) {
+            if (object == null) {
+                outputString += "null";
+            } else {
+                outputString += object;
+            }
+            if (index != m_values.length - 1) {
+                outputString += ",";
+            }
+            index++;
+        }
+        return outputString + "]";
+    }
+
     @Override public boolean equals(Object other) {
-        if(other instanceof Tuple) {
+        if (other instanceof Tuple) {
             return equals((Tuple) other);
         }
         return false;
     }
 
     @Override public int hashCode() {
-        return m_classes.hashCode() + 31 * m_values.hashCode();
+        return m_values.hashCode();
     }
 }
